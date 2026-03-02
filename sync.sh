4f3b2c666dfa70f96b3036100330fcc8ff836da2
foxxie911 <foxxie911.dev@proton.me>
2026-03-02 15:31:57 +0600

    rofi and oh-my-posh added

--- sync.sh
+++ sync.sh
@@ -11,47 +11,48 @@ folders[i3status]="$HOME/.config/i3status"
 folders[picom]="$HOME/.config/picom"
 folders[rofi]="$HOME/.config/rofi"
 folders[kitty]="$HOME/.config/kitty"
+folders[oh - my - posh]="$HOME/.config/oh-my-posh"
 folders[CustomBinary]="$HOME/.local/bin"
 folders[wallpapers]="$HOME/.wallpapers"
 
 # Iterate over folders and copy their contents to the current directory
 for folder in "${folders[@]}"; do
-    # Use rsync for a more efficient and robust copying mechanism
-    rsync -r "$folder" ./
-    echo "Copied: $folder"
+  # Use rsync for a more efficient and robust copying mechanism
+  rsync -r "$folder" ./
+  echo "Copied: $folder"
 done
 
 # Define a function to perform Git operations
 git_commit() {
-    local message="$1"
+  local message="$1"
 
-    # Stage all changes in the current directory
-    git add .
+  # Stage all changes in the current directory
+  git add .
 
-    # Commit changes with the provided message
-    git commit -m "$message"
+  # Commit changes with the provided message
+  git commit -m "$message"
 
-    # Push changes to the origin repository
-    git push -u origin master
+  # Push changes to the origin repository
+  git push -u origin master
 }
 
 # Parse command-line arguments
 while [[ "$#" =~ ^- ]]; do
-    case "$1" in
-    -m | --message)
-        shift
-            # Call the git_commit function with the message as an argument
-            git_commit "$1"
-        ;;
-    -v | --version)
-            echo "Version 1.2"
-        ;;
-    esac
+  case "$1" in
+  -m | --message)
     shift
+    # Call the git_commit function with the message as an argument
+    git_commit "$1"
+    ;;
+  -v | --version)
+    echo "Version 1.2"
+    ;;
+  esac
+  shift
 done
 
 # Handle remaining command-line arguments (if any)
-if [[ "$#" > 0 ]]; then
-    # Handle unknown or unsupported arguments
-    echo "Unknown argument: $1"
+if [[ "$#" -gt 0 ]]; then
+  # Handle unknown or unsupported arguments
+  echo "Unknown argument: $1"
 fi
